aed042c8e45bf8bbb25ebf3e3af4fa262c4e905e
TarunBasediya <tarunbasediya020@gmail.com>
2024-06-02 19:54:40 +0530

    Added build folder

---
+++ presidio/build/precache-manifest.4883707bb38d3a974e69b6265a442d56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66988ea8d9c4e27cb8f40f96be12dc86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eac9e918fc832dfc7d38",
+    "url": "/static/css/main.ca8d5c72.chunk.css"
+  },
+  {
+    "revision": "17fcf0f363da216a0c34",
+    "url": "/static/js/2.b59e4959.chunk.js"
+  },
+  {
+    "revision": "0c2bdb564318543e6f2622f136a18796",
+    "url": "/static/js/2.b59e4959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e0b29191e7df3fb68a",
+    "url": "/static/js/3.98e127e7.chunk.js"
+  },
+  {
+    "revision": "eac9e918fc832dfc7d38",
+    "url": "/static/js/main.c547db17.chunk.js"
+  },
+  {
+    "revision": "dfeb1b7076df77d79cea",
+    "url": "/static/js/runtime-main.7dca2fbd.js"
+  }
+]);
